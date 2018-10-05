d468f635b12ac953d39d26a7ac7998ad15117246
Tony Zharoff <tony.zharoff@gmail.com>
2018-10-05 11:06:41 -0700

    added registerServiceWorker with npm

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App';
-import RegisterServiceWorker from "./registerServiceWorker";
+import serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
     <Router>
@@ -12,4 +12,4 @@ ReactDOM.render(
     document.getElementById("root")
 );
 
-RegisterServiceWorker();
+serviceWorker();
